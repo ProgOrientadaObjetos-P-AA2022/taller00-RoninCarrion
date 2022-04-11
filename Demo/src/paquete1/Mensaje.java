e0fa96e049477f2a324aaf427afd80c22b75c069
RoninCarrion <92813788+RoninCarrion@users.noreply.github.com>
2022-04-11 09:33:05 -0500

    Clase01_RoninCarrion

--- Demo/src/paquete1/Mensaje.java
+++ Demo/src/paquete1/Mensaje.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package paquete1;
+import paquete4.Palabra;
 
 /**
  *
@@ -12,6 +13,8 @@ package paquete1;
 public class Mensaje {
 
     public static String obtenerFrase() {
-        return String.format("%s, una ciudad llena de arte y naturaleza", Palabra.obtenerPalabra());
+        return String.format("%s, una ciudad llena de arte y naturaleza"
+                , Palabra.obtenerPalabra());
     }
+
 }
